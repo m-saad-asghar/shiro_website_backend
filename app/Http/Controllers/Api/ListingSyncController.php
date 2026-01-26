3956e3e9d2d7bb965218390ae69cfdc2d5ec1c24
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2026-01-26 16:31:05 +0400

    fix synching

--- app/Http/Controllers/Api/ListingSyncController.php
+++ app/Http/Controllers/Api/ListingSyncController.php
@@ -39,7 +39,7 @@ class ListingSyncController extends Controller
                 'order' => [
                     ['column' => 0, 'dir' => 'asc'],
                 ],
-                'page' => 11,
+                'page' => 12,
             ]);
 
         if (!$listRes->successful()) {

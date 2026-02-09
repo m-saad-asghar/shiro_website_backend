491209f055d46fe9c9d457b86f93b587459ea194
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2026-02-09 16:32:30 +0400

    revert

--- config/mail.php
+++ config/mail.php
@@ -6,54 +6,59 @@ return [
     |--------------------------------------------------------------------------
     | Default Mailer
     |--------------------------------------------------------------------------
+    |
+    | This option controls the default mailer that is used to send all email
+    | messages unless another mailer is explicitly specified when sending
+    | the message. All additional mailers can be configured within the
+    | "mailers" array. Examples of each type of mailer are provided.
+    |
     */
+
     'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
     | Mailer Configurations
     |--------------------------------------------------------------------------
+    |
+    | Here you may configure all of the mailers used by your application plus
+    | their respective settings. Several examples have been configured for
+    | you and you are free to add your own as your application requires.
+    |
+    | Laravel supports a variety of mail "transport" drivers that can be used
+    | when delivering an email. You may specify which one you're using for
+    | your mailers below. You may also add additional mailers if needed.
+    |
+    | Supported: "smtp", "sendmail", "mailgun", "ses", "ses-v2",
+    |            "postmark", "resend", "log", "array",
+    |            "failover", "roundrobin"
+    |
     */
+
     'mailers' => [
-
-        'smtp' => [
+/*
+       'smtp' => [
             'transport' => 'smtp',
-
-            // For Gmail use smtp.gmail.com, for cPanel use mail.shiroestate.ae / mail.shirovps.com
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 465),
-
-            // 465 => ssl, 587 => tls
-            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
-
+            'scheme' => env('MAIL_SCHEME'),
+            'url' => env('MAIL_URL'),
+            'host' => env('MAIL_HOST', '127.0.0.1'),
+            'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-
             'timeout' => null,
-            'auth_mode' => null,
-
-            /*
-             * TEMP BYPASS (remove later):
-             * Some servers have PHP stream TLS issues even when openssl CLI works.
-             */
-            'stream' => [
-                'ssl' => [
-                    'allow_self_signed' => true,
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-
-                    // Optional: force TLS 1.2 if TLS1.3 causes issues
-                    // 'crypto_method' => STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT,
-                ],
-            ],
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
-
+*/
         'ses' => [
             'transport' => 'ses',
         ],
 
         'postmark' => [
             'transport' => 'postmark',
+            // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
+            // 'client' => [
+            //     'timeout' => 5,
+            // ],
         ],
 
         'resend' => [
@@ -76,25 +81,85 @@ return [
 
         'failover' => [
             'transport' => 'failover',
-            'mailers' => ['smtp', 'log'],
+            'mailers' => [
+                'smtp',
+                'log',
+            ],
             'retry_after' => 60,
         ],
 
         'roundrobin' => [
             'transport' => 'roundrobin',
-            'mailers' => ['ses', 'postmark'],
+            'mailers' => [
+                'ses',
+                'postmark',
+            ],
             'retry_after' => 60,
         ],
+
+        'smtp' => [
+    'transport' => 'smtp',
+    'host' => env('MAIL_HOST'),
+    'port' => env('MAIL_PORT'),
+    'encryption' => env('MAIL_ENCRYPTION'),
+    'username' => env('MAIL_USERNAME'),
+    'password' => env('MAIL_PASSWORD'),
+    'timeout' => null,
+    'auth_mode' => null,
+],
+
+        
+        
+        // 'smtp' => [
+        //     'transport' => 'smtp',
+        //     'host' => env('MAIL_HOST', 'mail.shirovps.com'),
+        //     'port' => env('MAIL_PORT', 465),
+        //     'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+        //     'username' => env('MAIL_USERNAME', 'noreply@shiroestate.ae'),
+        //     'password' => env('MAIL_PASSWORD'),
+        //     'timeout' => null,
+        //     'auth_mode' => null,
+
+        // ],
+
+        'log' => [
+            'transport' => 'log',
+        ],
+   
+
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
+    ],
+        
+
     ],
 
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
     |--------------------------------------------------------------------------
+    |
+    | You may wish for all emails sent by your application to be sent from
+    | the same address. Here you may specify a name and address that is
+    | used globally for all emails that are sent by your application.
+    |
     */
+    
+//     'stream' => [
+//     'ssl' => [
+//         'allow_self_signed' => true,
+//         'verify_peer' => false,
+//         'verify_peer_name' => false,
+//     ],
+// ],
+
+
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'noreply@shiroestate.ae'),
-        'name' => env('MAIL_FROM_NAME', 'Shiro Estate'),
+        'name' => env('MAIL_FROM_NAME', 'Shiro Properties'),
     ],
+
+
 
 ];

7674718d00677fef2927ae764fb6c5e91708ff09
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2026-02-10 12:27:37 +0400

    clean mail.php

--- config/mail.php
+++ config/mail.php
@@ -11,25 +11,16 @@ return [
             'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -t -i'),
         ],
 
-        'smtp' => [
-            'transport' => 'smtp',
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 25),
-            'encryption' => env('MAIL_ENCRYPTION'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'auth_mode' => null,
-        ],
-
         'log' => [
             'transport' => 'log',
             'channel' => env('MAIL_LOG_CHANNEL', 'stack'),
         ],
+
     ],
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'enquiry@shiroestate.ae'),
         'name' => env('MAIL_FROM_NAME', 'Shiro Estate'),
     ],
+
 ];

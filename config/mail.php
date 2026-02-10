675028acc333cbfd28ab0cdfc99676a45d32d46a
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2026-02-10 09:43:07 +0400

    change mail.php

--- config/mail.php
+++ config/mail.php
@@ -2,52 +2,35 @@
 
 return [
 
-    // 'default' => env('MAIL_MAILER', 'smtp'),
-    'default' => 'sendmail',
-
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     'mailers' => [
 
-       'smtp' => [
-  'transport' => 'smtp',
-  'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-  'port' => env('MAIL_PORT', 587),
-  'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-  'username' => env('MAIL_USERNAME'),
-  'password' => env('MAIL_PASSWORD'),
-  'timeout' => null,
-  'auth_mode' => null,
+        'smtp' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'auth_mode' => null,
 
-  // TEMP bypass if server keeps failing cert verify:
-  'stream' => [
-    'ssl' => [
-      'verify_peer' => false,
-      'verify_peer_name' => false,
-      'allow_self_signed' => false,
-    ],
-  ],
-],
-
-'sendmail' => [
-    'transport' => 'sendmail',
-    'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -t -i'),
-],
-
-
-
-        // 'sendmail' => [
-        //     'transport' => 'sendmail',
-        //     'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
-        // ],
+            // Optional (only if you REALLY need to bypass SSL checks)
+            // 'stream' => [
+            //     'ssl' => [
+            //         'allow_self_signed' => true,
+            //         'verify_peer' => false,
+            //         'verify_peer_name' => false,
+            //     ],
+            // ],
+        ],
 
         'log' => [
             'transport' => 'log',
             'channel' => env('MAIL_LOG_CHANNEL', 'stack'),
         ],
 
-        'array' => [
-            'transport' => 'array',
-        ],
     ],
 
     'from' => [
